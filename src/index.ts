20ffa1b912adbac6eb971537a63e38f06f48273b
Rodrigo de Oliveira Carvalho Soares <rodrigo.soares@expocaccer.com.br>
2022-11-19 11:58:11 -0300

    FEAT: Criação dos models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
-});
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
